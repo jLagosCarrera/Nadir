ae037ada660890a638e119835f5bb41dcbe14d17
Julio <julio.lagos.carrera@gmail.com>
2019-01-30 11:02:50 +0100

    Writing LevelGenerator class

---
+++ app/src/main/java/com/github/jlagoscarrera/nadir/LevelGenerator.java
@@ -0,0 +1,131 @@
+package com.github.jlagoscarrera.nadir;
+
+public class LevelGenerator {
+
+    private int levelSize = 4;
+    private Room[][] rooms;
+    private Room startRoom;
+    private Room endRoom;
+
+    public LevelGenerator() {
+        rooms = new Room[levelSize][levelSize];
+    }
+
+    /**
+     * Initializes all rooms empty.
+     */
+    private void initializeRooms() {
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[i].length; j++) {
+                rooms[i][j] = new Room(0, (i * levelSize) + j, j, i);
+            }
+        }
+    }
+
+    /**
+     * Generates the solution path.
+     */
+    private void generateRoomType() {
+        int currentX, currentY, prevX, prevY;
+
+        //We generate where the spawn room will be on the top row.
+        currentX = (int) (Math.random() * levelSize);
+        currentY = 0;
+        rooms[currentX][currentY].setType(1);
+        startRoom = rooms[currentX][currentY];
+
+        while (currentY < 4) {
+            boolean weGoDown;
+            int random = (int) (Math.random() * 100);
+
+            //40% chances going left, 40% chances going right, 20% chances going down
+            switch (random) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                case 32:
+                case 33:
+                case 34:
+                case 35:
+                case 36:
+                case 37:
+                case 38:
+                case 39:
+                    break;
+                case 40:
+                case 41:
+                case 42:
+                case 43:
+                case 44:
+                case 45:
+                case 46:
+                case 47:
+                case 48:
+                case 49:
+                case 50:
+                case 51:
+                case 52:
+                case 53:
+                case 54:
+                case 55:
+                case 56:
+                case 57:
+                case 58:
+                case 59:
+                case 60:
+                case 61:
+                case 62:
+                case 63:
+                case 64:
+                case 65:
+                case 66:
+                case 67:
+                case 68:
+                case 69:
+                case 70:
+                case 71:
+                case 72:
+                case 73:
+                case 74:
+                case 75:
+                case 76:
+                case 77:
+                case 78:
+                case 79:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/github/jlagoscarrera/nadir/Room.java
@@ -0,0 +1,48 @@
+package com.github.jlagoscarrera.nadir;
+
+public class Room {
+
+    private int type;
+    private int id;
+    private int x;
+    private int y;
+
+    public Room(int type, int id, int x, int y) {
+        this.type = type;
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
